0353b80f91368d40e28218bdb208e5fa1a1da460
workonfire <kolucki62@gmail.com>
2020-08-19 18:31:34 +0200

    Boolean improvements

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from os import system
 from colorama import init, deinit, Fore, Style
 from server import Server, ServerDoesNotExist
 from time import strftime
+import platform
 
 __VERSION__ = '1.0.4'
 __AUTHOR__ = 'workonfire'
@@ -14,6 +15,9 @@ def color_print(color, text):
 
 
 def main():
+    if platform.system() == 'Windows':
+        system('title CraftserveScraper')
+
     banner = ["   ______           ______                          ",
               "  / ____/________ _/ __/ /_________  ______   _____ ",
               " / /   / ___/ __ `/ /_/ __/ ___/ _ \\/ ___/ | / / _ \\",
@@ -33,27 +37,27 @@ def main():
         except ValueError:
             color_print(Fore.RED, "Incorrect format.")
 
-    logging = input("Enable logging? (y/n) ")
-    verbosity = input("Enable verbosity? (y/n) ")
-    filtering = input("Enable filtering? (y/n) ")
+    logging = input("Enable logging? (y/n) ").lower() == 'y'
+    verbosity = input("Enable verbosity? (y/n) ").lower() == 'y'
+    filtering = input("Enable filtering? (y/n) ").lower() == 'y'
     special_query = False
-    online_filter = 'n'
-    max_players_filter = 'n'
+    online_filter = False
+    max_players_filter = False
     max_players_filter_value = 0
     server_type_filter = 'all'
-    online_mode_filter = 'n'
-    plugins_filter = 'n'
+    online_mode_filter = False
+    plugins_filter = False
     filtered_plugins = []
 
-    if filtering == 'y':
-        online_filter = input("Filter by online servers? (y/n) ").lower()
-        max_players_filter = input("Filter by max players? (y/n) ").lower()
-        if max_players_filter == 'y':
+    if filtering:
+        online_filter = input("Filter by online servers? (y/n) ").lower() == 'y'
+        max_players_filter = input("Filter by max players? (y/n) ").lower() == 'y'
+        if max_players_filter:
             max_players_filter_value = int(input("Max players filter value (e.g. 10): "))
         server_type_filter = input("Filter by server type? (grass/diamond/km/all) ").lower()
-        online_mode_filter = input("Filter by online mode? (y/n) ").lower()
-        plugins_filter = input("Filter by plugins? (y/n) ").lower()
-        if plugins_filter == 'y':
+        online_mode_filter = input("Filter by online mode? (y/n) ").lower() == 'y'
+        plugins_filter = input("Filter by plugins? (y/n) ").lower() == 'y'
+        if plugins_filter:
             print("Please type the plugin name, e.g. EssentialsX.")
             print("If you want to finish, type \"end\".")
             while True:
@@ -62,13 +66,13 @@ def main():
                     break
                 else:
                     filtered_plugins.append(filtered_plugin)
-        if online_mode_filter == 'y' or plugins_filter == 'y':
+        if online_mode_filter or plugins_filter:
             special_query = True
             color_print(Fore.RED,
                         "WARN: Filtering by online mode or plugins or version might cause the program to query the "
                         "server longer.")
             color_print(Fore.RED, "It is better to not use this option, unless you really have to.")
-    if verbosity == 'n' and logging == 'n':
+    if not verbosity and not logging:
         color_print(Fore.RED, "WARN: Logging and verbosity is disabled. What is the point of that..?")
 
     color_print(Fore.CYAN, "\nScraper launched!\n")
@@ -79,18 +83,18 @@ def main():
             server = Server(server_id, special_query=special_query)
 
             try:
-                if filtering == 'y':
-                    if online_filter == 'y' and not server.running:
+                if filtering:
+                    if online_filter and not server.running:
                         continue
-                    if max_players_filter == 'y' and not max_players_filter_value == server.max_online:
+                    if max_players_filter and not max_players_filter_value == server.max_online:
                         continue
                     if server_type_filter != 'all' and not server_type_filter == str(server.type).lower():
                         continue
-                    if online_mode_filter == 'y' and not server.online_mode:
+                    if online_mode_filter and not server.online_mode:
                         continue
-                    if plugins_filter == 'y' and not any(plugin in server.plugins for plugin in filtered_plugins):
+                    if plugins_filter and not any(plugin in server.plugins for plugin in filtered_plugins):
                         continue
-                if logging == 'y':
+                if logging:
                     with open('logs.txt', 'a') as log_file:
                         log_file.write(f"[{strftime('%d.%m.%Y %H:%M:%S')}] ----- MATCH FOUND! -----\n" +
                                        f"ID: {server.id}\n" +
@@ -105,7 +109,7 @@ def main():
                             log_file.write(f"Plugin list: {', '.join([str(plugin) for plugin in server.plugins])}")
                         log_file.write("\n")
 
-                if verbosity == 'y':
+                if verbosity:
                     color_print(Fore.GREEN, "----- MATCH FOUND! -----")
                     print(f"ID: {server.id}")
                     print(f"Name: {server.name}")
@@ -123,7 +127,7 @@ def main():
             except (AttributeError, UnicodeEncodeError):
                 color_print(Fore.RED, f"Something weird happened. Skipping {server.id}...")
         except ServerDoesNotExist:
-            if verbosity == 'y':
+            if verbosity:
                 color_print(Fore.RED, f"This server does not exist. Skipping {server_id}...")
 
 
@@ -131,4 +135,5 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        system("pause")
+        if platform.system() == 'Windows':
+            system("pause")
